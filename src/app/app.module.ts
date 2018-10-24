4d90408c6fd3ca88c4adf384f42309e29b72a927
Erik Mauricio Moreno Vázquez <e.mauricio.moreno@gmail.com>
2018-10-24 11:14:31 -0500

    Fin de efectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 
+//Ngrx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
+import { appReducers } from './store/app.reducer';
+import { effectsArr } from './store/effects';
+
+//environment
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
@@ -15,6 +26,12 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot(effectsArr),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
     SharedModule,
     UsuariosModule
   ],
